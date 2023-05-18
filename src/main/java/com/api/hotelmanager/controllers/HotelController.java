3a7b3922fce758ba0e6ce82717b75fd17b1fa5d1
--global <--global>
2023-05-18 02:14:07 +0100

    Created controller and service for Hotel Entity

---
+++ src/main/java/com/api/hotelmanager/controllers/HotelController.java
@@ -0,0 +1,55 @@
+package com.api.hotelmanager.controllers;
+
+import java.net.URI;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.api.hotelmanager.dtos.GuestResponse;
+import com.api.hotelmanager.dtos.HotelRequest;
+import com.api.hotelmanager.dtos.HotelResponse;
+import com.api.hotelmanager.entities.Hotel;
+import com.api.hotelmanager.services.HotelService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(value = "/api/hotel")
+@RequiredArgsConstructor
+public class HotelController {
+	
+	private final HotelService hotelService;
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<HotelResponse> getHotel(@PathVariable Long id) {
+		HotelResponse hotelResponse = hotelService.getHotel(id);
+		return ResponseEntity.ok(hotelResponse);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<HotelResponse>> getAll(Pageable page){
+		Page<HotelResponse> hotelResponsePage = hotelService.getAll(page);
+		return ResponseEntity.ok(hotelResponsePage);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Hotel> createHotel(@RequestBody HotelRequest hotelRequest,
+			UriComponentsBuilder uriComponentsBuilder) {
+		Hotel hotel = hotelService.createHotel(hotelRequest);
+		URI uri = uriComponentsBuilder
+				.path("/api/hotel/{id}")
+				.buildAndExpand(hotel
+						.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(hotel);
+	}
+
+}
--- src/main/java/com/api/hotelmanager/dtos/HotelRequest.java
+++ src/main/java/com/api/hotelmanager/dtos/HotelRequest.java
@@ -1,6 +1,10 @@
 package com.api.hotelmanager.dtos;
 
+import java.util.List;
+
+import com.api.hotelmanager.entities.Room;
+
 import lombok.Builder;
 
 @Builder
-public record HotelRequest (String name, String address, Integer stars) {}
+public record HotelRequest (String name, String address, Integer stars, List<Room> rooms) {}
